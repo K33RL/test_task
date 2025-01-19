f4cbab22c541fcd1307860174fb852b86a210024
K.Perevozov <k.perevozov@gmail.com>
2025-01-19 21:48:37 +0300

    Added:  log

--- src/main/java/test/app/account/AccountApplication.java
+++ src/main/java/test/app/account/AccountApplication.java
@@ -1,15 +1,18 @@
 package test.app.account;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class AccountApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AccountApplication.class, args);
+    log.info("APPLICATION STARTED");
   }
 
 }
