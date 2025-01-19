fe6d79efaf96607e9ed38900b7d527ede92b59bd
K.Perevozov <k.perevozov@gmail.com>
2025-01-19 21:47:20 +0300

    Added:  swagger documentation

--- src/main/java/test/app/account/controller/AuthController.java
+++ src/main/java/test/app/account/controller/AuthController.java
@@ -1,5 +1,7 @@
 package test.app.account.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,13 @@ import test.app.account.security.AuthenticationService;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Название контроллера", description = "Описание контроллера")
 public class AuthController {
 
   private final AuthenticationService authenticationService;
 
   @PostMapping("/sign-in")
+  @Operation(summary = "Аутентификация пользователя", description = "Позволяет сгенерировать токен для пользователя")
   public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
     return authenticationService.signIn(request);
   }
--- src/main/java/test/app/account/controller/dto/SignInRequest.java
+++ src/main/java/test/app/account/controller/dto/SignInRequest.java
@@ -1,11 +1,14 @@
 package test.app.account.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Данные необходимые для аутентификации пользователя")
 public class SignInRequest {
 
+  @Schema(description = "Email", example = "email@email.com")
   private String email;
-
+  @Schema(description = "Password", example = "q1w2e3")
   private String password;
 }
